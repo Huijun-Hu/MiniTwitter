f26e2a6eb2911256000a83b2a333b30648920bcd
Huijun Hu <huijunhu@HuijundeiMac.local>
2023-11-09 20:48:13 -0800

    edit Singleton

--- MiniTwitter/NotificationService.java
+++ MiniTwitter/NotificationService.java
@@ -1,5 +1,23 @@
 package MiniTwitter;
 
-public class NotificationService {
+import java.util.*;
 
+public class NotificationService {
+    private List<User> followers;
+
+    public NotificationService() {
+        followers = new ArrayList<>();
+    }
+
+    public void subscribe(User u) {
+        followers.add(u);
+    }
+
+    public void unsubcribe(User u) {
+        followers.remove(u);
+    }
+
+    public void newFeed(User u, String msg) {
+        followers.forEach(user -> user.update(u, msg));
+    }
 }
--- MiniTwitter/UserGroup.java
+++ MiniTwitter/UserGroup.java
@@ -1,10 +1,21 @@
 package MiniTwitter;
 
+import java.util.*;
+
 public class UserGroup {
     private int id;
+    private String GroupName;
+
+    public UserGroup(int id, String name) {
+        this.id = id;
+        this.GroupName = name;
+    }
 
 }
-// A user group has an unique ID, which can be used to group users. A user group can
-// contain any number of users. The same user can only be included in one group. Of
-// course, a user group can contain other user groups recursively. There is always a root
+// A user group has an unique ID, which can be used to group users. A user group
+// can
+// contain any number of users. The same user can only be included in one group.
+// Of
+// course, a user group can contain other user groups recursively. There is
+// always a root
 // group called Root to include everythin
